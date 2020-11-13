0500dad44cafaab3684fabc82fb992f9bd0fe2af
Mohammed Adil <adilmsrit@gmail.com>
2020-11-13 09:29:49 +0400

    1

--- src/main/java/pSight/DriverFactory.java
+++ src/main/java/pSight/DriverFactory.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 public class DriverFactory {
 
     private static WebDriver driver;
@@ -17,6 +19,8 @@ public class DriverFactory {
         if (driver == null) {
             System.setProperty("webdriver.chrome.driver", "./src/main/resources/drivers/chromedriver.exe");
             driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
         }
         return driver;
     }
